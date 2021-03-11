f14abb4bb49490e68e417c129a05643f722c15b5
Siddharth <Siddharthsingh7898@gmail.com>
2021-03-11 19:25:42 +0530

    Program and qr code image added

---
+++ qrcode.py
@@ -0,0 +1,10 @@
+import qrcode
+
+#link of data : like video, image, song etc.
+# image = qrcode.make("link")
+
+#example
+image = qrcode.make("https://www.youtube.com/watch?v=kJQP7kiw5Fk")  
+
+#name of file and its extension. like JPG, PNG.
+image.save("qr", "PNG") 
